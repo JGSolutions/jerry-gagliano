cca725933e4a8e2714d695cd42b44ef99f6afab2
Jerry Gagliano <jerrygag@gmail.com>
2022-09-15 21:26:26 -0400

    created blog item component

---
+++ components/simple-blog-item/simple-blog-item.js
@@ -0,0 +1,10 @@
+import styles from './simple-blog-item.module.css'
+
+
+export default function SimpleBlogItem() {
+    return (
+        <div className={styles.wrapper}>
+
+        </div>
+    )
+}
