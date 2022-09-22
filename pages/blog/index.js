908daa05eaf49415ffa8309ed65d841b5948bf19
Jerry Gagliano <jerrygag@gmail.com>
2022-09-21 20:08:10 -0400

    modified  title tag

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -6,7 +6,7 @@ export default function Blog() {
     return (
         <>
             <Head>
-                <title>Jerry Gagliano - Web development content and tips.</title>
+                <title>My experiences about Web development and tips.</title>
                 <meta name="description" content="Providing helpful web development content" />
                 <meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1,user-scalable=no"></meta>
                 <link rel="icon" href="/favicon.ico" />
