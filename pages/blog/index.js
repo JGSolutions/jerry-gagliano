a4be36f4fc285d227d41deca2b95e9e2e4866841
Jerry Gagliano <jerrygag@gmail.com>
2022-09-22 08:27:28 -0400

    creating styles and layout

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,6 +1,8 @@
 import utilStyles from '../../styles/utils.module.css';
+import styles from './blog.module.css';
 import Head from 'next/head'
 import Header  from '../../components/header/header';
+import Footer  from '../../components/footer/footer';
 
 export default function Blog() {
     return (
@@ -15,8 +17,16 @@ export default function Blog() {
             <div className={utilStyles.topSection}>
                 <div className="container">
                     <Header/>
+
+                    <section className={utilStyles.headerPageSection}>
+                        <h1 className={utilStyles.headerPageTitle}>My Blog</h1>
+                        <p>Latest and updated news about projects, development tips and general experiences about the web.</p>
+                    </section>
                 </div>
             </div>
+
+            <main style={{ height: '100vh', minHeight: '600px'}}></main>
+            <Footer/>
         </>
     )
 }
