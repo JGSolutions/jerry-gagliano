ec911ba49a6731e5d3c91c12f92f61c3ccc86777
Jerry Gagliano <jerrygag@gmail.com>
2022-09-25 16:32:50 -0400

    moved the viewport to app

--- pages/_document.js
+++ pages/_document.js
@@ -9,7 +9,6 @@ function MyDocument() {
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"></link>
                 <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet"></link>
                 <link href="https://fonts.googleapis.com/css2?family=Source+Code+Pro&display=swap" rel="stylesheet"></link>
-                <meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1,user-scalable=no"></meta>
                 <link rel="icon" href="/favicon.ico" />
                 <meta name="theme-color" content="#121111" />
             </Head>
