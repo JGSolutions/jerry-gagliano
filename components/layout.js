78e833a13f36f8d40b31cb282ebb75ecaf93dcf3
Jerry Gagliano <jerrygag@gmail.com>
2022-09-25 12:41:48 -0400

    cleaning up layout

--- components/layout.js
+++ components/layout.js
@@ -3,6 +3,8 @@ import Head from 'next/head'
 import styles from './layout.module.css'
 // import utilStyles from '../styles/utils.module.css'
 // import Link from 'next/link'
+import Header from '../components/header/header';
+import Footer from '../components/footer/footer';
 
 const name = '[Your Name]'
 export const siteTitle = 'Next.js Sample Website'
@@ -13,19 +15,14 @@ export default function Layout({ children, home }) {
       <Head>
         <link rel="icon" href="/favicon.ico" />
         <meta name="description" content="Learn how to build a personal website using Next.js" />
-        <meta
-          property="og:image"
-          content={`https://og-image.vercel.app/${encodeURI(
-            siteTitle
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-        />
+        <meta property="og:image" content=""/>
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
-      <header className={styles.header}>
-       
-      </header>
+      <Header></Header>
       <main>{children}</main>
+
+      <Footer></Footer>
     </div>
   )
 }
