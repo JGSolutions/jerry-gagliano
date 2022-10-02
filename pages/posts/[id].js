7c2d81c2fdd13872ffc27a2ea5d63bf89e3b1146
Jerry Gagliano <jerrygag@gmail.com>
2022-10-02 08:15:11 -0400

    added back button in blog article

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -6,6 +6,7 @@ import styles from './post.module.css'
 import Header from '../../components/header/header';
 import Footer from '../../components/footer/footer';
 import { generateTagsElements  } from '../../lib/utils'
+import Link from 'next/link'
 
 export async function getStaticProps({ params }) {
     const postData = await getPostData(params.id);
@@ -41,32 +42,30 @@ export default function Post({ postData }) {
         <div className="container-lg">
           <Header/>
           <section className={utilStyles.headerPageSection}>
+
+            <Link href="/blog" passHref>
+              <a className={utilStyles.back} rel="noopener noreferrer">
+                &larr; Go Back
+              </a>
+            </Link>
             <h1 className={utilStyles.headerPageTitle}>{postData.title}</h1>
             <p className={utilStyles.headerPageSlogan}>
               {postData.description}
             </p>
           </section>
         </div>
-        {/* <div style={{position: 'absolute'}}>
-          <Image
-              src={postData.coverImage}
-              height={400}
-              width={750}
-              alt=""
-            />
-        </div> */}
       </div>
 
       <main>
         <div className="container-lg">
           <div className="row">
-            <div className={`col col-sm-12 col-md-8 col-lg-8 ${styles.content}`}>
+            <div className={`col-xs-12 col-md-8 col-lg-8 ${styles.content}`}>
               <article className={styles.article}>
                 <div className={styles.articleText} dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
               </article>
             </div>
 
-            <div className={`col col-sm-12 col-md-4 col-lg-4 ${styles.sideContent}`}>
+            <div className={`col-xs-12 col-md-4 col-lg-4 ${styles.sideContent}`}>
                 <section className={styles.details}>
                   <p className={styles.authorDate}>
                     By {postData.author} / <FormatDate dateString={postData.updateDate} />
