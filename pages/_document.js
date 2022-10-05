723ec2c08264d1dcfdaf16e3613401fda41ecee2
Jerry Gagliano <jerrygag@gmail.com>
2022-10-05 18:54:00 -0400

    changed favicon

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,7 @@ function MyDocument() {
     return (
         <Html>
             <Head>
-                <link rel="icon" href="/favicon.ico" />
+                <link rel="icon" href="/favicon.svg" sizes="any" type="image/svg+xml" />
                 <link rel="preconnect" href="https://fonts.googleapis.com"></link>
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"></link>
                 <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet"></link>
