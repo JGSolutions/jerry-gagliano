8cda90eab2ef4550d809501054f4ef6ca7da6377
Jerry Gagliano <jerrygag@gmail.com>
2022-10-12 19:00:25 -0400

    adding profile  pic

--- components/hero/hero.js
+++ components/hero/hero.js
@@ -1,26 +1,39 @@
 import styles from './hero.module.css';
 import Link from 'next/link'
+import Image from 'next/image'
 import utilStyle from '../../styles/utils.module.css'
 
 export default function HeroSection() {
     return (
         <section className={styles.heroSection}>
-            <h1 className={styles.h1}>
-                Hi, I&#39;m Jerry Gagliano. <br/>
-                <span className={styles.heroText}>
-                Bringing <span className={styles.heroTextHightlight}>ideas</span> to <span className={styles.heroTextHightlight}>reality!</span>
-                </span>
-            </h1>
+            <div>
+                <h1 className={styles.h1}>
+                    Hi, I&#39;m Jerry Gagliano. <br/>
+                    <span className={styles.heroText}>
+                    Bringing <span className={styles.heroTextHightlight}>ideas</span> to <span className={styles.heroTextHightlight}>reality!</span>
+                    </span>
+                </h1>
 
-            <p className={styles.slogan}>
-                Full stack developer crafting solid and scalable frontend products with great user experiences.
-            </p>
+                <p className={styles.slogan}>
+                    Full stack developer crafting solid and scalable front-end Progressive Web Apps.
+                </p>
 
-            <Link href="/blog">
-                <a className={utilStyle.button} rel="noopener noreferrer">
-                    Read My Blog
-                </a>
-            </Link>
+                <Link href="/blog">
+                    <a className={utilStyle.button} rel="noopener noreferrer">
+                        Read My Blog
+                    </a>
+                </Link>
+            </div>
+
+            <div className={styles.profileWrapper}>
+                <Image
+                    src="/jerry-pic.jpeg"
+                    className={styles.profilePic}
+                    height={207}
+                    width={207}
+                    alt="jerry gagliano"
+                />
+            </div>
         </section>
     )
 }
