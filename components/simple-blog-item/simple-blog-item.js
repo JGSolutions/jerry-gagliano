eecba994101c2eb4ab0ec75efde810bbe65c84f1
Jerry Gagliano <jerrygag@gmail.com>
2022-12-28 15:46:19 -0500

    added recent blogs item

--- components/simple-blog-item/simple-blog-item.js
+++ components/simple-blog-item/simple-blog-item.js
@@ -1,6 +1,7 @@
 import styles from './simple-blog-item.module.css'
 import FormatDate from '../date';
 import TagItem from '../tag/tag';
+import Image from 'next/image';
 
 export default function SimpleBlogItem({title, description, image, date, tags}) {
     const tagItems = tags.split(',').map((string, index) =>
@@ -10,7 +11,7 @@ export default function SimpleBlogItem({title, description, image, date, tags}) 
         <div className={styles.wrapper}>
             <div className={styles.titleSection}>{title}</div>
             <div className={styles.imageSection}>
-                <img src={image} alt={title}/>
+                <Image src={image} fill alt={title} quality={45} priority  />
             </div>
             <div className={styles.contentSection}>
                 <div className={styles.description}>
