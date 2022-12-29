850a8bccf1ac7f1b6624b0fe96950141f46e1c32
Jerry Gagliano <jerrygag@gmail.com>
2022-12-29 10:25:43 -0500

    set menu desktop display

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -10,51 +10,37 @@ export default function Header() {
 
     return (
         <>
-            <header className={styles.header}>
+            <header className={styles.header}>                
                 <div className={styles.logoContainer}>
+                    <Link href="/" passHref rel="noopener noreferrer">
+                        <Image src="/jg-logo.svg" alt="Jerry Gagliano logo" width={100} height={50} priority></Image>
+                    </Link>
 
-                <Link href="/" passHref rel="noopener noreferrer">
-                    <Image src="/jg-logo.svg" alt="Jerry Gagliano logo" width={100} height={50}></Image>
-                </Link>
-                    {/* <nav className={styles.nav}>
+                    <nav className={styles.desktopNav}>
                         <ul role='list' className={styles.menu}>
                             <li>
-                                <Link href="https://fitvitfitness.com">
-                                    <a className={styles.link}>About</a>
-                                </Link>
+                                <Link href="https://fitvitfitness.com/blog/" className={styles.link}>Blog</Link>
                             </li>
+
                             <li>
-                                <Link href="https://fitvitfitness.com">
-                                    <a className={styles.link}>
-                                        Blog
-                                    </a>
-                                </Link>
-                            </li>
-                            <li>
-                                <Link href="https://fitvitfitness.com">
-                                    <a className={styles.link}>
-                                        Uses
-                                    </a>
-                                </Link>
+                                <Link href="https://fitvitfitness.com" className={styles.link}>Portfolio</Link>
                             </li>
                         </ul>
-                    </nav> */}
+                    </nav>
                 </div>
-                
-                <button className={styles.menuButton} onClick={() => setOpenPanel(!open)}>
-					<Image
-						src="/menu.svg"
-						height={28}
-						width={32}
-						alt="menu"
-						/>
-				</button>
-                
-                <Link href="https://docs.google.com/document/d/1sunyIYQNLZfWAAQq1kbRtHBX0ivRrte58iJFHFpQxuc/edit" passHref legacyBehavior>
-                    <a className={utilStyle.button}  target="_blank" rel="noopener noreferrer">
-                        My Resume
-                    </a>
+
+                <Link href="https://docs.google.com/document/d/1sunyIYQNLZfWAAQq1kbRtHBX0ivRrte58iJFHFpQxuc/edit" passHref className={utilStyle.button}  target="_blank" rel="noopener noreferrer">
+                    My Resume
                 </Link>
+
+                <button className={styles.menuButton} onClick={() => setOpenPanel(!open)}>
+                    <Image
+                        src="/menu.svg"
+                        height={28}
+                        width={32}
+                        alt="menu"
+                        />
+                </button>
             </header>
 
             <SideNav open={open}>
