03d1144fb4af8e418d1416c986cfb1d757832eae
Jerry Gagliano <jerrygag@gmail.com>
2023-01-06 09:30:58 -0500

    removed packages

--- components/blog-layout/blog-layout.tsx
+++ components/blog-layout/blog-layout.tsx
@@ -1,4 +1,4 @@
-import { getAllPostIds, getPostData } from '../../lib/posts';
+
 import Head from 'next/head';
 import FormatDate from '../../components/date';
 import utilStyles from '../../styles/utils.module.css'
@@ -8,22 +8,6 @@ import Footer from '../../components/footer/footer';
 import { generateTagsElements  } from '../../lib/utils'
 import Link from 'next/link'
 
-// export async function getStaticProps({ params }) {
-//     const postData = await getPostData(params.id);
-//     return {
-//         props: {
-//             postData,
-//         },
-//     };
-// }
-
-// export async function getStaticPaths() {
-//   const paths = getAllPostIds();
-//   return {
-//     paths,
-//     fallback: false,
-//   };
-// }
 
 export default function BlogLayout({ meta, children }) {
   const tags =  generateTagsElements(meta.tags);
