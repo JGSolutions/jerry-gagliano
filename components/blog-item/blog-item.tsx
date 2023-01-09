6d740b7566dd11731e743e2d5d09c398693d417c
Jerry Gagliano <jerrygag@gmail.com>
2023-01-08 20:21:36 -0500

    changed h3 tag

--- components/blog-item/blog-item.tsx
+++ components/blog-item/blog-item.tsx
@@ -14,7 +14,7 @@ export default function BlogItem({id, title, description, image, date, updatedDa
                 </div>
 
                 <Link href={`/posts/${id}`} className={styles.titleSection}>
-                    <h3 className={styles.h3}>{title}</h3>
+                    <h2 className={styles.h3}>{title}</h2>
                 </Link>
 
                 {/* <div className={styles.updatedDate}>
