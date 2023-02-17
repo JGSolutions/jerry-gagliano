999c2658fd491fb2df4367c2b1d6f31f4e6f0513
Jerry Gagliano <jerrygag@gmail.com>
2023-02-17 12:03:16 -0500

    fixed domain for redirects

--- components/blog-layout/blog-layout.tsx
+++ components/blog-layout/blog-layout.tsx
@@ -17,8 +17,8 @@ export default function BlogLayout({ meta, children }) {
   let canonicalUrl;
 
   if (env === "production") {
-    url ='https://www.jgsolutions.ca';
-    canonicalUrl ='https://www.jgsolutions.ca';
+    url ='https://jgsolutions.ca';
+    canonicalUrl ='https://jgsolutions.ca';
   } else {
     url ='http://localhost:3000';
   }
